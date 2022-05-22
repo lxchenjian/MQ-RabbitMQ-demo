a7b34ccca3348f14aa6aef1e2b90ad893a01490c
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-05-22 18:30:54 +0800

    建立项目

---
+++ src/main/java/com/rabbitmq/mqrabbitmqdemo/MqRabbitMqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.mqrabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqRabbitMqDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqRabbitMqDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rabbitmq/mqrabbitmqdemo/MqRabbitMqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.mqrabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqRabbitMqDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
